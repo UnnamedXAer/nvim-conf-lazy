5f87d631b94f85c80728b809f765f08ef57d4bba
UnnamedXAer <UnnamedXAer@gmail.com>
2024-10-24 13:03:33 +0200

    dap dC / dm

--- lua/me/lazy.lua
+++ lua/me/lazy.lua
@@ -910,10 +910,18 @@ require("lazy").setup({
 			)
 			vim.keymap.set("n", "<leader>br", "<cmd>lua require'dap'.clear_breakpoints()<cr>")
 			vim.keymap.set("n", "<leader>ba", "<cmd>Telescope dap list_breakpoints<cr>")
+
 			vim.keymap.set("n", "<leader>dc", "<cmd>lua require'dap'.continue()<cr>")
 			vim.keymap.set("n", "<leader>dj", "<cmd>lua require'dap'.step_over()<cr>")
 			vim.keymap.set("n", "<leader>dk", "<cmd>lua require'dap'.step_into()<cr>")
 			vim.keymap.set("n", "<leader>do", "<cmd>lua require'dap'.step_out()<cr>")
+			vim.keymap.set(
+				"n",
+				"<leader>dC",
+				"<cmd>lua require'dap'.run_to_cursor()<cr>",
+				{ desc = "[d]ap | run to [C]ursor" }
+			)
+
 			vim.keymap.set("n", "<leader>dd", function()
 				require("dap").disconnect()
 				require("dapui").close()
@@ -936,6 +944,17 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>de", function()
 				require("telescope.builtin").diagnostics({ default_text = ":E:" })
 			end)
+
+			vim.keymap.set(
+				"n",
+				"<leader>dm",
+				function()
+					---@diagnostic disable-next-line: missing-fields
+					require("dapui").eval(nil, { enter = true })
+				end,
+				-- '<Cmd>lua require("dapui").eval()<CR>',
+				{ desc = "dapui - eval expression under the curson" }
+			)
 		end,
 	},
 
