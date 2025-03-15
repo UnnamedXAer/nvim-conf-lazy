f2f55f39ff99fc1f90df60ad8f7780f60d9af276
UnnamedXAer <unnamedxaer.git@gmail.com>
2025-03-15 18:09:32 +0100

    todo

--- lua/plugins/nvim-dap-python.lua
+++ lua/plugins/nvim-dap-python.lua
@@ -1,3 +1,31 @@
+-- TODO: find venv
+-- -- 1. find venv folder in current dir or 1 level deeper (venv/ or proj/venv)
+-- local function find_venv(start_path) -- Finds the venv folder required for LSP
+--   -- Check current directory (if venv folder is at root)
+--   local venv_path = start_path .. "/venv"
+--   if vim.fn.isdirectory(venv_path) == 1 then
+--     return venv_path
+--   end
+--   -- Check one level deeper (e.g if venv is in proj/venv)
+--   local handle = vim.loop.fs_scandir(start_path)
+--   if handle then
+--     while true do
+--       local name, type = vim.loop.fs_scandir_next(handle)
+--       if not name then
+--         break
+--       end
+--       if type == "directory" then
+--         venv_path = start_path .. "/" .. name .. "/venv"
+--         if vim.fn.isdirectory(venv_path) == 1 then
+--           return venv_path
+--         end
+--       end
+--     end
+--   end
+--
+--   return nil
+-- end
+
 return {
   "mfussenegger/nvim-dap-python",
   ft = "python",
