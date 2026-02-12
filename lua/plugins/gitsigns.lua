a8a65d122dc96cdbd9e1a8a33d4ae1a91a79a951
UnnamedXAer <UnnamedXAer@gmail.com>
2026-02-12 13:06:07 +0100

    update gitsigns config

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -34,6 +34,8 @@ return { -- Adds git related signs to the gutter, as well as utilities for manag
           return "<Ignore>"
         end, { expr = true, desc = "navigate to previous hunk (Gitsigns)" })
 
+        -- for full list of available actions, see: https://github.com/lewis6991/gitsigns.nvim/blob/main/doc/gitsigns.txt
+
         map("n", "hp", gs.preview_hunk, { desc = "preview hunk (Gitsigns)" })
 
         map("n", "<leader>hs", gs.stage_hunk, { desc = "stage (unstage) hunk((Gitsigns)" })
@@ -41,12 +43,6 @@ return { -- Adds git related signs to the gutter, as well as utilities for manag
           gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
         end, { desc = "stage (unstage) hunk - selected line (Gitsigns)" })
 
-        map(
-          "n",
-          "<leader>hu",
-          gs.undo_stage_hunk,
-          { desc = "undo stage hunk (this only reverses last stage/unstage action, similar what 'u' does) (Gitsigns)" }
-        )
         map("n", "<leader>hr", gs.reset_hunk, { desc = "reset hunk - discards the changes! (Gitsigns)" })
         map("n", "<leader>hS", gs.stage_buffer, { desc = "stage buffer (Gitsigns)" })
         -- map("n", "<leader>hR", gs.reset_buffer, { desc = "reset buffer (Gitsigns)" })
@@ -65,8 +61,9 @@ return { -- Adds git related signs to the gutter, as well as utilities for manag
       },
       preview_config = {
         border = "rounded",
-        style = "minimal",
+        -- style = "minimal",
         relative = "cursor",
+        focusable = true,
       },
     })
   end,
